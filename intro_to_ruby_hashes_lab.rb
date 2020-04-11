23477dbbb6ec07fb03dee3183b93bd1889ce65b0
Ryan Letterio <rletterio42@gmail.com>
2020-04-11 23:39:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,8 @@
 def base_hash
-base = {:railroads => {}}
+return base = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
-base_hash
 base[:railroads] = {pieces: 4, rent_in_dollars: {}, names: {}}
 end
 
